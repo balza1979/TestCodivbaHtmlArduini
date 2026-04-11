1529bc069acae4dc6fcec0ed1377ee5b043b5339
balza1979 <balza1979@live.com>
2026-04-11 14:45:48 +0200

    Initialize Firebase app and database configuration

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,28 @@
 
+// ============================================================
+//  IMPORT FIREBASE
+// ============================================================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+// ============================================================
+//  CONFIGURAZIONE FIREBASE
+// ============================================================
+const firebaseConfig = {
+    apiKey: "AIzaSyB1_lem0tKd5SQeuD7xs8Dj3Ey77MUUhDY",
+    authDomain: "programmazione-4d38e.firebaseapp.com",
+    databaseURL: "https://programmazione-4d38e-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "programmazione-4d38e",
+    storageBucket: "programmazione-4d38e.appspot.com",
+    messagingSenderId: "390860890072",
+    appId: "1:390860890072:web:5199a3f3c80f5b95a5dcad"
+};
+
+// ============================================================
+//  INIZIALIZZAZIONE
+// ============================================================
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+
+// Esporto anche ref, set, get, child così puoi usarli in editor.js
+export { ref, set, get, child };
